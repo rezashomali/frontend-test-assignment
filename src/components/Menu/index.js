4e9a9183093f8262ea2ffb069f0189e2918d972c
reza shomali nezhad <r.shomalinezhad@gmail.com>
2020-10-05 14:23:53 +0200

    add material ui to menu

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { ToggleButtonGroup, ToggleButton } from "@material-ui/lab";
 
 const Menu = () => {
   return (
     <div>
-      <p>
-        <Link to="/table">Table</Link>
-      </p>
-      <p>
-        <Link to="/chart">Chart</Link>
-      </p>
+      <ToggleButtonGroup exclusive aria-label="text alignment">
+        <ToggleButton value="/table">
+          <Link to="/table">Table</Link>
+        </ToggleButton>
+        <ToggleButton value="/chart">
+          <Link to="/chart">Chart</Link>
+        </ToggleButton>
+      </ToggleButtonGroup>
     </div>
   );
 };
