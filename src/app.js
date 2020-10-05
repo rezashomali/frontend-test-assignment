1586c349f614619cc24c755301340d89118f578b
reza shomali nezhad <r.shomalinezhad@gmail.com>
2020-10-05 14:44:54 +0200

    change app component to use material ui

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container, Grid } from "@material-ui/core";
 import Menu from "./components/Menu";
 import RiskLevelSelector from "./components/RiskLevelSelector";
 import Table from "./components/Table";
@@ -7,28 +8,43 @@ import Chart from "./components/Chart";
 
 const App = () => {
   const [riskLevel, setRiskLevel] = useState(3);
+  const [initialSum, setInitialSum] = useState(1000);
+  const [cones, setCones] = useState();
+  const [calculation, setCalculation] = useState();
 
   return (
     <Router>
-      <div>
-        <Menu />
-        <RiskLevelSelector
-          onChangeRiskLevel={(e) => setRiskLevel(e.target.value)}
-        />
-        <Route
-          exact
-          path="/"
-          component={() => <Table riskLevel={riskLevel} />}
-        />
-        <Route
-          path="/table"
-          component={() => <Table riskLevel={riskLevel} />}
-        />
-        <Route
-          path="/chart"
-          component={() => <Chart riskLevel={riskLevel} />}
-        />
-      </div>
+      <Container maxWidth="md" className="container">
+        <Grid container>
+          <Grid item xs={12}>
+            <Menu />
+          </Grid>
+          <Grid item xs={6}>
+            <RiskLevelSelector
+              riskLevel={riskLevel}
+              onChangeRiskLevel={(e) => setRiskLevel(e.target.value)}
+            />
+          </Grid>
+
+          <Route exact path="/">
+            <Grid item xs={12}>
+              <Table riskLevel={riskLevel} />
+            </Grid>
+          </Route>
+
+          <Route path="/table">
+            <Grid item xs={12}>
+              <Table riskLevel={riskLevel} />
+            </Grid>
+          </Route>
+
+          <Route path="/chart">
+            <Grid item xs={12}>
+              <Chart riskLevel={riskLevel} />
+            </Grid>
+          </Route>
+        </Grid>
+      </Container>
     </Router>
   );
 };
