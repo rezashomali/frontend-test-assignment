c6b1b8d7164f40e67858bcc80c87fd49477abaa7
reza shomali nezhad <r.shomalinezhad@gmail.com>
2020-10-05 14:24:22 +0200

    change riskLevelSelector to functional

--- src/components/RiskLevelSelector/index.js
+++ src/components/RiskLevelSelector/index.js
@@ -1,51 +1,41 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-class RiskLevelSelector extends React.Component {
+const RiskLevelSelector = ({
+  onChangeRiskLevel,
+  minRiskLevel,
+  maxRiskLevel,
+}) => {
+  var defultRiskl = 10;
+  const options = [];
+  for (let k = 1; k <= maxRiskLevel; ++k) {
+    options.push(
+      <option key={k} value={k}>
+        {k}
+      </option>
+    );
+  }
 
-    constructor(props) {
-        super(props);
-        this.onChange = this.onChange.bind(this);
-    }
-
-    onChange(event) {
-        let {onChangeRiskLevel} = this.props;
-        var riskLevel = parseInt(event.target.value);
-        onChangeRiskLevel(riskLevel);
-    }
-
-    render() {
-
-        const {minRiskLevel, maxRiskLevel} = this.props;
-        var defultRiskl = 10;
-        const options = [];
-        for(let k=1; k<=maxRiskLevel; ++k) {
-            options.push(
-                <option key={k} value={k}>{k}</option>
-            );
-        }
-
-        return (
-            <div>
-                Risk level:
-                <select onChange={this.onChange} defaultValue={defultRiskl}>
-                    {options}
-                </select>
-            </div>
-        );
-    }
-}
+  return (
+    <div>
+      Risk level:
+      <select onChange={onChangeRiskLevel} defaultValue={defultRiskl}>
+        {options}
+      </select>
+    </div>
+  );
+};
 
 RiskLevelSelector.defaultProps = {
-    minRiskLevel: 3,
-    maxRiskLevel: 25,
-    onChangeRiskLevel: () => {}
+  minRiskLevel: 3,
+  maxRiskLevel: 25,
+  onChangeRiskLevel: () => {},
 };
 
 RiskLevelSelector.propTypes = {
-    minRiskLevel: PropTypes.number,
-    maxRiskLevel: PropTypes.number,
-    onChangeRiskLevel: PropTypes.func
+  minRiskLevel: PropTypes.number,
+  maxRiskLevel: PropTypes.number,
+  onChangeRiskLevel: PropTypes.func,
 };
 
 export default RiskLevelSelector;
