3dc31d7810aee13597295874aa56cd2cd3b36c01
reza shomali nezhad <r.shomalinezhad@gmail.com>
2020-10-05 15:10:01 +0200

    get Data from api and send to Table component

--- src/app.js
+++ src/app.js
@@ -1,17 +1,31 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Container, Grid } from "@material-ui/core";
+import { getCalculation } from "./utils/utils";
+
 import Menu from "./components/Menu";
 import RiskLevelSelector from "./components/RiskLevelSelector";
 import Table from "./components/Table";
 import Chart from "./components/Chart";
 import "./App.css";
+import cones from "../cones";
 
 const App = () => {
   const [riskLevel, setRiskLevel] = useState(3);
   const [initialSum, setInitialSum] = useState(1000);
   const [cones, setCones] = useState();
   const [calculation, setCalculation] = useState();
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/cones")
+      .then((response) => response.json())
+      .then((data) => setCones(data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  useEffect(() => {
+    cones && setCalculation(getCalculation(cones, riskLevel));
+  }, [cones, riskLevel]);
 
   return (
     <Router>
@@ -29,13 +43,13 @@ const App = () => {
 
           <Route exact path="/">
             <Grid item xs={12}>
-              <Table riskLevel={riskLevel} />
+              {calculation && <Table {...calculation} />}
             </Grid>
           </Route>
 
           <Route path="/table">
             <Grid item xs={12}>
-              <Table riskLevel={riskLevel} />
+              {calculation && <Table {...calculation} />}
             </Grid>
           </Route>
 
